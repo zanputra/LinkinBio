0912dab367ee2e3fff39c5ec59071047bd7a0aa6
Fauzan Harlyanto Putra <83821807+zanputra@users.noreply.github.com>
2021-10-21 09:29:23 +0700

    remodel the web

--- js/main.js
+++ js/main.js
@@ -18,12 +18,11 @@ $(document).ready(function(){
     // owl-carousel
     $('.owl-carousel').owlCarousel({
         loop: true,
-        autoplay: false,
+        autoplay: true,
         autoplayTimeout: 3000,
         dots: false,
         nav: false,
         responsive: responsive
-        
     });
 
     // AOS Instance
